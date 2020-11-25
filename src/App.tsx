bcf13ffdd750ed4e694c175e021d402516d6404e
burbuda <pikmin@yandex.ru>
2020-11-25 20:34:37 +0300

    Some fixes

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// LIBs
+import React from 'react';
+
+// Componenst
+import Canvas from './components/canvas';
+
+import classes from './styles.module.css';
+const App = () => {
+  return (
+    <div className={classes.App}>
+      <Canvas/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/canvas/variables.ts
@@ -0,0 +1,10 @@
+export const ballRadius = 10;
+export const brickRowCount = 3;
+export const brickColumnCount = 3;
+export const brickWidth = 75;
+export const brickHeight = 20;
+export const brickPadding = 10;
+export const brickOffsetTop = 30;
+export const brickOffsetLeft = 30;
+export const paddleHeight = 10;
+export const paddleWidth = 75;
